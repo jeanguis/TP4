8958e1e99e945eecdbfc8d978e52651b16762b94
Jean <jean.guis@mines-paristech.fr>
2015-11-26 00:28:37 -0300

    CAN'T FIND THE @&"é&ç ERROR

--- MataCerebros.py
+++ MataCerebros.py
@@ -2,11 +2,11 @@ from Listas import *
 
 class MataCerebros(object):
 
-	def __init__(self, cintaSize = 300000):
+	def __init__(self, cintaSize = 10):
 		self.cintaList = ListaDoble()
 		for i in xrange(1,cintaSize):
 			self.cintaList.append(0)
-		self.currentCelda = self.cintaList.head
+		self.currentCelda = self.cintaList.head.prox.prox
 		self.actionDict = {"+": self.add, "-": self.remove, ">": self.up, "<": self.down, ".": self.printascii };
 
 
@@ -21,11 +21,23 @@ class MataCerebros(object):
 			return self.currentCelda.dato
 
 	def up(self):
-		self.currentCelda=self.currentCelda.prox
+		if self.currentCelda.prox:
+			self.currentCelda=self.currentCelda.prox
+		else:
+			self.cintaList.append(0)
+			self.currentCelda=self.currentCelda.prox
 		return self.currentCelda
 
 	def down(self):
-		self.currentCelda=self.currentCelda.prev
+		if self.currentCelda.prev:
+			self.currentCelda=self.currentCelda.prev
+		else:
+			print "VALOR:",self.currentCelda.dato
+			print "VALOR NEXT:",self.currentCelda.prox.dato
+			print "VALOR NEXT2:",self.currentCelda.prox.prox.dato
+			print "VALOR NEXT3:",self.currentCelda.prox.prox.prox.dato
+			print self
+			exit()
 		return self.currentCelda
 
 	def printascii(self):
@@ -44,11 +56,13 @@ class MataCerebros(object):
 		while not colas.es_vacia():
 			#raw_input()
 			action=colas.desencolar()
-			print "ACTION:",action
 			if type(action) is str:
 				self.command(action)
 			else:
 				cicleCelda = self.currentCelda
+				#cicleCola = Cola()
+				#while not action.es_vacia():
+				#	cicleCola.encolar(action.desencolar())
 				cicleCola = action
 				while not cicleCelda.dato == 0:
 					newCola = Cola()
